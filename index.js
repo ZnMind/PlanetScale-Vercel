f699ff7654af209ac42569c0a780cc88294b9954
DKProtocol <damann1072@gmail.com>
2022-12-09 10:21:15 -0600

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect()
+app.use(express.json())
+app.use(cors());
+
+app.get('/:year?/:split?', (req, res) => {
+  const year = req.params.year;
+  const split = req.params.split;
+  connection.query(`select * from lcs where year = ? and split = ?`, [year, split], function(err, results, fields) {
+    if (err) throw err
+    res.send(results);
+  })
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is running on port: ${port}`))
+
+module.exports = app;
